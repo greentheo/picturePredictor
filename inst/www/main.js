5c16704f0328aad082738d23c7a82bdd5b7c1406
Theodore Van Rooy <munkey906@gmail.com>
2015-01-15 17:28:11 -0700

    js configuring

--- inst/www/main.js
+++ inst/www/main.js
@@ -1,30 +1,29 @@
 // a set of js functions that run the picture prediction app
-dataTablesBB = Backbone.Model.extend({
+pictures = Backbone.Model.extend({
   defaults:{
-    truckLoadDataTable: null,
-    viableAltTable: null
-  },
-  initialize: function(){
-    this.on('change:truckLoadDataTable', function(model){
-      model.get('truckLoadDataTable').rows().draw();
-    })
+    pictureName: null
     
   },
-  setTruckLoadTable: function(table){
-    this.set({truckLoadDataTable: table})
+  initialize: function(){
+    this.on('change:pictureName', function(model){
+      //TODO functions that save the last picture that was clicked on triggering
+    //the changes that move the last picture into the main picture frame
+    //and two new suggestions below it...
+    })
   },
-  setViableAltTable: function(table){
-    this.set({viableAltTable: table})
-  }
-
+  pictureClick: function(pictureName){
+    this.set({pictureClicked: pictureName});
+    this.
+  },
+ 
 });
 
 
-var Tables;
+var pictureBB;
 //var editor;
 
 $(document).ready(function() {
-  Tables = new dataTablesBB();
+  pictureBB = new pictures();
   
   //and do the rest of the app
   
@@ -32,6 +31,10 @@ $(document).ready(function() {
     console.log('p1Click');
   });
   $('.secondpicture').click(function(event){
+    //TODO make an OPENCPU call here that basically logs the event 
+    //Then trigger the function in the backbone model that the p2 was the last one clicked
+    
+    pictureBB.pictureClick( 'the pictures name')
     console.log('p2Click');
   })
   $('.sidepicture').click( function(event){
